66cfc80d43ef8e5bf494c7d80c377e80e6bdc2f1
kokol12 <kokoleismail886@gmail.com>
2023-03-29 23:24:52 +0300

    leet code two sum optimized solution

---
+++ 1-two_sum.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    approaching the two sum problem
+    given a list and a target value. find two values in the list that
+    add up to the targe
+    eg:
+        input:
+            list: [1, 4, 8, 2]
+            target: 3
+        output: [0, 3]
+
+    solution:
+        optimizing the code to minimize on its time complexity
+        we are going to check throught the list and get a value at a given index and
+        get the difference from the target then check if the difference is available on
+        list and then return the two indices
+"""
+
+
+def two_sum(nums, target):
+    """ Looping throug the list to get indices"""
+    for i in range(len(nums)):
+        difference = target - nums[i]
+        try:
+            diff_index = nums.index(difference)
+            return [i, diff_index]
+        except IndexError as e:
+            return (e.__name__)
+
+
+nums = [1, 4, 8, 2]
+target = 3
+
+print(two_sum(nums, target))
